d1d3970112af839f6347c3a862e025e8a1a8f123
Vi_Barile <vinibarile@gmail.com>
2024-11-11 14:26:44 -0300

    CRUD 1 e implementação de update e delete

--- posts/views.py
+++ posts/views.py
@@ -1,43 +1,56 @@
 from .models import Post
 from django.urls import reverse, reverse_lazy
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from django.http import HttpResponse
-from .temp_data import post_data
+from django.http import HttpResponseRedirect, HttpResponse
 
-def detail_post(request, post_id):
-    post = post_data[post_id - 1]
-    return HttpResponse(
-        f'Detalhes do livro {post["name"]} ({post["release_year"]})')
 
 def list_posts(request):
-    context = {"post_list": post_data}
+    post_list = Post.objects.all()
+    context = {"post_list": post_list}
     return render(request, 'posts/index.html', context)
 
 def detail_post(request, post_id):
-    context = {'post': post_data[post_id - 1]}
+    post = get_object_or_404(Post, pk=post_id)
+    context = {'post': post}
     return render(request, 'posts/detail.html', context)
 
 def search_posts(request):
     context = {}
     if request.GET.get('query', False):
-        context = {
-            "post_list": [
-                m for m in post_data
-                if request.GET['query'].lower() in m['name'].lower()
-            ]
-        }
+        search_term = request.GET['query'].lower()
+        post_list = Post.objects.filter(name__icontains=search_term)
+        context = {"post_list": post_list}
     return render(request, 'posts/search.html', context)
 
 def create_post(request):
     if request.method == 'POST':
-        post_data.append({
-            'name': request.POST['name'],
-            'release_year': request.POST['release_year'],
-            'capa_url': request.POST['capa_url']
-        })
-        return HttpResponseRedirect(
-            reverse('posts:detail', args=(len(post_data), )))
+        post_name = request.POST['name']
+        post_release_year = request.POST['release_year']
+        post_content = request.POST['content']
+        post_capa_url = request.POST['capa_url']
+        post = Post(name=post_name, release_year=post_release_year, content=post_content, capa_url=post_capa_url)
+        post.save()
+        return HttpResponseRedirect(reverse('posts:detail', args=[post.id]))
     else:
         return render(request, 'posts/create.html', {})
+    
+def update_post(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if request.method == "POST":
+        post.name = request.POST['name']
+        post.release_year = request.POST['release_year']
+        post.content = request.POST['content']
+        post.capa_url = request.POST['capa_url']
+        post.save()
+        return HttpResponseRedirect(
+            reverse('posts:detail', args=(post.id, )))
+    context = {'post': post}
+    return render(request, 'posts/update.html', context)
+
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if request.method == "POST":
+        post.delete()
+        return HttpResponseRedirect(reverse('posts:index'))
+    context = {'post': post}
+    return render(request, 'posts/delete.html', context)
